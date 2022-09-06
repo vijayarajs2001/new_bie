0cf617d9a97996d328e8b914ee93e281d96d9073
vijayarajs2001 <vijayarajs2001@gmail.com>
2022-09-06 18:34:28 +0530

    hi

--- tennis_problem.js
+++ tennis_problem.js
@@ -3,109 +3,93 @@ tennisMatch = [
         name: "Player1",
         rank: 1,
         NumberOfMatchesPlayed: 455,
-        numberOfMatchsWon: 440,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 440
     }, {
         name: "Player2",
         rank: 2,
         NumberOfMatchesPlayed: 423,
-        numberOfMatchsWon: 403,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 403
     }, {
         name: "Player3",
         rank: 3,
         NumberOfMatchesPlayed: 401,
-        numberOfMatchsWon: 376,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 376
     }, {
         name: "Player4",
         rank: 4,
         NumberOfMatchesPlayed: 332,
-        numberOfMatchsWon: 302,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 302
     }, {
         name: "Player5",
         rank: 5,
         NumberOfMatchesPlayed: 302,
-        numberOfMatchsWon: 267,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 267
     }, {
         name: "Player6",
         rank: 7,
         NumberOfMatchesPlayed: 286,
-        numberOfMatchsWon: 246,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 246
     }, {
         name: "Player7",
         rank: 7,
         NumberOfMatchesPlayed: 225,
-        numberOfMatchsWon: 180,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 180
     }, {
         name: "Player8",
         rank: 8,
         NumberOfMatchesPlayed: 200,
-        numberOfMatchsWon: 151,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 151
     }
     , {
         name: "Player9",
         rank: 9,
         NumberOfMatchesPlayed: 195,
-        numberOfMatchsWon: 142,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 142
     }, {
         name: "Player10",
         rank: 10,
         NumberOfMatchesPlayed: 183,
-        numberOfMatchsWon: 126,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 126
     }, {
         name: "Player11",
         rank: 11,
         NumberOfMatchesPlayed: 181,
-        numberOfMatchsWon: 121,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 121
     }, {
         name: "Player12",
         rank: 12,
         NumberOfMatchesPlayed: 172,
-        numberOfMatchsWon: 106,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 106
     }, {
         name: "Player13",
         rank: 13,
         NumberOfMatchesPlayed: 172,
-        numberOfMatchsWon: 102,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 102
     }, {
         name: "Player14",
         rank: 14,
         NumberOfMatchesPlayed: 166,
-        numberOfMatchsWon: 94,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 94
     }, {
         name: "Player15",
         rank: 15,
         NumberOfMatchesPlayed: 155,
-        numberOfMatchsWon: 78,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 78
     }, {
         name: "Player16",
         rank: 16,
         NumberOfMatchesPlayed: 150,
-        numberOfMatchsWon: 70,
-        // rateOfWinning:(numberOfMatchsWon/(NumberOfMatchesPlayed*rank))
+        numberOfMatchsWon: 70
     }
 ]
-numberOfRounds = Math.sqrt(tennisMatch.length)
 count = 0
-// for (i = 1; i <= numberOfRounds; i++) {
+//-------------------------------------------------------------
 function rateOfWinningAddition(tennisMatch) {
     for (l of tennisMatch) {
         l["rateOfWinning"] = [l.numberOfMatchsWon / (l.NumberOfMatchesPlayed * l.rank) * 100]
     }
 }
+//-------------------------------------------------------------
 function GrandSlamDrawMatchScheduling(tennisMatch) {
     arr = []
     for (i of tennisMatch) {
@@ -115,13 +99,12 @@ function GrandSlamDrawMatchScheduling(tennisMatch) {
     return scheduling(arr)
 }
 // console.log(arr)
-///////////////////////////////////////////////////////
+//-------------------------------------------------------------
 function scheduling(arr) {
     array = []
     for (i = 0; i < arr.length / 2; i++) {
         array.push([arr[i], arr[arr.length - 1 - i]])
     }
-    // console.log(array)
     schedule1 = []
     array2 = []
     for (i = 0; i < array.length; i++) {
@@ -135,17 +118,14 @@ function scheduling(arr) {
         schedule1.push(i)
     }
     console.log("ROUND ", count += 1)
-    // console.log(schedule1).
-    // console.table(schedule1)
     sch = []
     for (let index = 0; index < schedule1.length; index++) {
         sch.push(schedule1[index][0] + ' vs ' + schedule1[index][1])
-
     }
     console.table(sch)
     return (winningStrategy(schedule1))
 }
-///////////////////////////////////////////////////////
+//-------------------------------------------------------------
 function winningStrategy(schedule1) {
     emptArray = []
     for (i = 0; i < schedule1.length; i++) {
@@ -154,7 +134,7 @@ function winningStrategy(schedule1) {
     }
     return normalScheduling(emptArray)
 }
-///////////////////////////////////////////////////////
+//-------------------------------------------------------------
 function rateOfWinningRatio(str) {
     for (v of tennisMatch) {
         if (v.name == str) {
@@ -163,13 +143,13 @@ function rateOfWinningRatio(str) {
         }
     }
 }
+//-------------------------------------------------------------
 function comparison(str1, str2) {
     player1WinningRate = rateOfWinningRatio(str1)
     player2WinningRate = rateOfWinningRatio(str2)
     if ((Math.floor(player1WinningRate - player2WinningRate) >= 49) && (Math.floor(player1WinningRate - player2WinningRate) < 50)) {
         emptyArray = [str1, str2]
         return emptyArray[Math.floor(Math.random() * 2)]
-
     }
     else {
         if (Number(player1WinningRate) > Number(player2WinningRate)) {
@@ -180,9 +160,8 @@ function comparison(str1, str2) {
         }
     }
 }
-///////////////////////////////////////////////////////
+//-------------------------------------------------------------
 function normalScheduling(emptArray) {
-
     if (emptArray.length > 1) {
         count += 1
         schedule2 = []
@@ -217,12 +196,9 @@ function normalScheduling(emptArray) {
         }
         schedule1 = schedule2
         return winningStrategy(schedule1)
-
     }
     else {
         console.log("The winner is", emptArray[0])
-        // console.log(tennisMatch)
     }
-
 }
 GrandSlamDrawMatchScheduling(tennisMatch)
